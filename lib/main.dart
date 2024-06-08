fd79314f82a23bdeafd035dc11639e75045c84a1
ankurt02 <ankurt5202@gmail.com>
2024-06-08 11:42:22 +0530

    added device preview

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,23 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weatherapp/screens/home.screen.dart';
 import 'package:device_preview/device_preview.dart';
 
-void main()  async {
-  await dotenv.load(fileName : "assets/.env");
-  runApp(
-  DevicePreview(
-    // enabled: !kReleaseMode,
-    builder: (context) => MyApp(), // Wrap your app
-  ),
-);}
 
-// void main() async {
-//   await dotenv.load(fileName: "assets/.env");
-//   runApp(const MyApp());
-// }
+// to use the device previewer, uncomment the below code
+// void main()  async {
+//   await dotenv.load(fileName : "assets/.env");
+//   runApp(
+//   DevicePreview(
+//     // enabled: !kReleaseMode,
+//     builder: (context) => MyApp(), // Wrap your app
+//   ),
+// );}
+
+
+// when using device preview, comment out the below code, and use the above main() function
+void main() async {
+  await dotenv.load(fileName: "assets/.env");
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
